a335a48ba354f8cb57892164c80d58556311ea95
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-10 15:09:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// function superBowlWin(item){
+//     for (let name of item) {
+//       if(name.result==="W"){
+//         console.log(name.year)
+//       } 
+//     }
+//   }
+function superbowlWin(record){
+    let result = record.find(record=>record.result==="W")
+    if(result===true){
+        return record.year;
+    }else{
+        return undefined;
+    }
+ 
+}
+function superBowlWin(sadReality){
+    let result = record.find(record=>record.result==="N/A")
+    result = undefined;
+    return result;
+}
